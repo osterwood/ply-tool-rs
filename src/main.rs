548f60ff21560a4d12d2173de62a79b2f14dc43f
Chris Osterwood <osterwood@gmail.com>
2019-03-24 11:18:58 -0400

    Allow color to be specified at RGB value

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,37 @@ use rand::prelude::*;
 use std::process::Command;
 use std::f64;
 use std::io::{BufRead, BufReader};
+use std::str::FromStr;
 
 #[derive(Debug)]
 struct Vertex {
     x: f64,
     y: f64,
     z: f64,
+}
+
+#[derive(Debug, PartialEq)]
+struct RGB {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl FromStr for RGB {
+    type Err = std::num::ParseIntError;
+
+    // Parses a color hex code of the form '#rRgGbB..' into an
+    // instance of 'RGB'
+    fn from_str(hex_code: &str) -> Result<Self, Self::Err> {
+    
+        // u8::from_str_radix(src: &str, radix: u32) converts a string
+        // slice in a given base to u8
+        let r: u8 = u8::from_str_radix(&hex_code[1..3], 16)?;
+        let g: u8 = u8::from_str_radix(&hex_code[3..5], 16)?;
+        let b: u8 = u8::from_str_radix(&hex_code[5..7], 16)?;
+
+        Ok(RGB { r, g, b })
+    }
 }
 
 fn parse_line(line: &String) -> Vertex {
@@ -83,7 +108,7 @@ fn main() {
     let mut scale = 0.01_f64;
     let mut angle = 0_f64;
     let mut outfile = "scan.png".to_string();
-    let mut color = 0_u8;
+    let mut color = "#000000".to_string();
     let mut bright = 0_i8;
 
     // this block limits scope of borrows by ap.refer() method
@@ -99,7 +124,7 @@ fn main() {
         ap.refer(&mut angle)
             .add_option(&["--angle"], Store, "Amount to rotate the image (in degrees)");
         ap.refer(&mut color)
-            .add_option(&["--color"], Store, "Color of the image, RGB as 0 or 1 (default black or 000)");
+            .add_option(&["--color"], Store, "Color of the image, in form '#RRGGBB' (default black)");
         ap.refer(&mut bright)
             .add_option(&["--bright"], Store, "Brightness change on the final image (-100 to 100)");
         ap.refer(&mut outfile)
@@ -121,26 +146,12 @@ fn main() {
 
     let mut image: image::ImageBuffer<image::Rgba<u8>, _> = image::ImageBuffer::new(xmax, ymax);
 
-    let mut r = 0_u8;
-    let mut g = 0_u8;
-    let mut b = 0_u8;
-
-    if color >= 100 {
-        r = 255;
-        color = color - 100;
-    }
-    if color >= 10 {
-        g = 255;
-        color = color - 10;
-    }
-    if color >= 1 {
-        b = 255;
-    }
+    let rgb = RGB::from_str(&color).unwrap(); 
 
     // Iterate over the coordinates and pixels of the image
     // and set all pixels to the transparent color specified
     for (_x, _y, pixel) in image.enumerate_pixels_mut() {
-        *pixel = image::Rgba([r,g,b,0]);
+        *pixel = image::Rgba([rgb.r,rgb.g,rgb.b,0]);
     }
 
     let file = std::fs::File::open(path).unwrap();
@@ -164,7 +175,7 @@ fn main() {
                 a = a + 10;
             }
 
-            image.put_pixel(x, y, image::Rgba([r,g,b,a]));
+            image.put_pixel(x, y, image::Rgba([rgb.r,rgb.g,rgb.b,a]));
         }
 
         if in_body == false && line == "end_header" {
